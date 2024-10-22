ca1e6b0e77352d5530411a71dab4c0d6baa6a773
Jeriel Sanao <sanaojeriellian@gmail.com>
2024-10-22 17:32:38 +0800

    Revise Lets Check

--- sanao_week2_letscheck.py
+++ sanao_week2_letscheck.py
@@ -1,14 +1,16 @@
 # Week 2 Let's Check
 
 # Problem 1
-user_input = input("Enter a phrase or write a sentence: ")
+user_input1 = input("Enter a phrase or write a sentence: ")
 
-print(f"Reversed: {user_input[::-1]}")
+print(f"Reversed: {user_input1[::-1]}")
 
 # Problem 2
-input_to_list = list(user_input)
+user_input2 = input("\nEnter a phrase or write a sentence: ")
+input_to_list = list(user_input2)
 print(f"Output in list: {input_to_list}")
 
 # Problem 3
-reversed_string = ' '.join(user_input.split()[::-1])  # Split the string into words, reverse, and join them back
+user_input3 = input("\nEnter a phrase or write a sentence: ")
+reversed_string = ' '.join(user_input3.split()[::-1])  # Split the string into words, reverse, and join them back
 print("Output:", reversed_string)
